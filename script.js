80ebaf7920280549722dec1e0ed161bb148d55b2
Doan vo van trong <trongtk24052003@gmail.com>
2023-10-05 10:32:32 +0700

    secondary commit

--- script.js
+++ script.js
@@ -2,8 +2,48 @@
 
 var shortcut_page = [
     {
-        'hinh_anh' : 'https://scontent.fdad3-6.fna.fbcdn.net/v/t39.30808-6/315274924_1094241955308376_7133570935767374608_n.jpg?stp=c6.0.64.64a_cp0_dst-jpg_p64x64&_nc_cat=1&ccb=1-7&_nc_sid=3be523&_nc_ohc=CV6tavenCm8AX-KaERp&_nc_ht=scontent.fdad3-6.fna&oh=00_AfDTRKvVb6Ui4kasTyynw9lTlkbaM2dJ59qzk_KeGn9Ysg&oe=6522F084',
-        'tieu_de_trang' : 'Phòng trọ Đà Nẵng' 
+        'hinh_anh': 'http://127.0.0.1:5500/images/daihocduytan.jpg',
+        'tieu_de_trang': 'DTU - Đại Học Duy Tân Đà Nẵng 2023'
+    },
+    {
+        'hinh_anh': 'http://127.0.0.1:5500/images/phongtrodanang.jpg',
+        'tieu_de_trang': 'CHỢ PC GAMING - GEAR 2ND ĐÀ NẴNG'
+    },
+    {
+        'hinh_anh': 'https://scontent.fdad3-6.fna.fbcdn.net/v/t39.30808-6/315274924_1094241955308376_7133570935767374608_n.jpg?stp=c6.0.64.64a_cp0_dst-jpg_p64x64&_nc_cat=1&ccb=1-7&_nc_sid=3be523&_nc_ohc=CV6tavenCm8AX-KaERp&_nc_ht=scontent.fdad3-6.fna&oh=00_AfDTRKvVb6Ui4kasTyynw9lTlkbaM2dJ59qzk_KeGn9Ysg&oe=6522F084',
+        'tieu_de_trang': 'Sinh viên Duy Tân Đà Nẵng'
+    },
+    {
+        'hinh_anh': 'http://127.0.0.1:5500/images/phongtrodannang.jpg',
+        'tieu_de_trang': 'Phòng trọ Đà Nẵng'
+    },
+    {
+        'hinh_anh': 'https://scontent.fhan2-3.fna.fbcdn.net/v/t39.2081-6/115803868_1632927190200218_7726110434222976102_n.png?stp=c5.5.50.50a_dst-png_p56x56&_nc_cat=1&ccb=1-7&_nc_sid=b29aad&_nc_ohc=JfPVtOUewtkAX_RSZMT&_nc_ht=scontent.fhan2-3.fna&oh=00_AfDeBlP0wLt_Z-gCfdyhCROgAhEmvEQTF_dY0D2J6coxTQ&oe=651DAFD8',
+        'tieu_de_trang': 'Cờ Caro'
+    },
+    {
+        'hinh_anh': 'https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/310274620_182329834309683_189006801584307188_n.jpg?stp=c34.0.64.64a_cp0_dst-jpg_p64x64&_nc_cat=101&ccb=1-7&_nc_sid=3be523&_nc_ohc=39ODu2nHzH0AX9pBBCT&_nc_ht=scontent.fdad3-4.fna&oh=00_AfBzymPR492Ny-jG1oXczz12RRxIxXr69AzNUzRsEJshYA&oe=652298E3',
+        'tieu_de_trang': 'PHÒNG TRỌ ĐÀ NẴNG GIÁ RẺ'
+    },
+    {
+        'hinh_anh': 'https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-6/341322652_205376498868218_4593150216512516633_n.jpg?stp=c25.0.64.64a_cp0_dst-jpg_p64x64&_nc_cat=110&ccb=1-7&_nc_sid=3be523&_nc_ohc=SBjsdCnAXuUAX_uBtnU&_nc_ht=scontent.fdad3-1.fna&oh=00_AfC1Bf64P6l99Y6-k8id8ZNlrOg8q9xgVwFqvhW45tZw-w&oe=6522A90D',
+        'tieu_de_trang': 'VIỆC LÀM - ĐÀ NẴNG'
+    },
+    {
+        'hinh_anh': 'https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/313209665_526415922829139_1931573599479211662_n.png?stp=c25.0.64.64a_cp0_dst-png_p64x64&_nc_cat=107&ccb=1-7&_nc_sid=3be523&_nc_ohc=vP8jlGpRzUkAX_4VW7H&_nc_ht=scontent.fdad3-4.fna&oh=00_AfBWoML5dK3GXMEt2M-9kOUSNziOhGEpupE-alqTlqnZtA&oe=6522BEA8',
+        'tieu_de_trang': 'Học lập trình web (F8 - Fullstack.edu.vn)'
+    },
+    {
+        'hinh_anh': 'https://scontent.fdad3-1.fna.fbcdn.net/v/t1.6435-9/166828405_1884571921709660_867543459382074909_n.jpg?stp=c26.0.64.64a_cp0_dst-jpg_p64x64&_nc_cat=103&ccb=1-7&_nc_sid=ac9ee4&_nc_ohc=a4vGozGd8SUAX8xOylc&_nc_ht=scontent.fdad3-1.fna&oh=00_AfBDMQenxUrwQ6qPFcbE0O0dNhO_kvSDFsjkeM0tt-NQlQ&oe=654511BF',
+        'tieu_de_trang': 'Làm thuê đồ án - Đồ án tốt nghiệp - Code thuê đồ án'
+    },
+    {
+        'hinh_anh': 'https://scontent.fdad3-6.fna.fbcdn.net/v/t39.30808-6/348984604_231452436186672_8174797555701630279_n.jpg?stp=c41.0.64.64a_cp0_dst-jpg_p64x64&_nc_cat=100&ccb=1-7&_nc_sid=3be523&_nc_ohc=iqZs5pjG-MYAX8esXtj&_nc_ht=scontent.fdad3-6.fna&oh=00_AfBCacxPk9Vf-w2ph0nrrxoMbjE1OmKyYqBiwGShC_0-Ew&oe=65221D85',
+        'tieu_de_trang': 'Thực tập IT Đà Nẵng - Việc làm CNTT Đà Nẵng'
+    },
+    {
+        'hinh_anh': 'https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-6/240655429_4059163764195718_3652059374269714525_n.jpg?stp=c35.0.64.64a_cp0_dst-jpg_p64x64&_nc_cat=108&ccb=1-7&_nc_sid=3be523&_nc_ohc=z9TliGgM7toAX-8jf_O&_nc_ht=scontent.fdad3-1.fna&oh=00_AfD3AkpQwmn1UMgPNt9QNwBbyEZ1tK7rPMxTRKZA775h9A&oe=6522EC0E',
+        'tieu_de_trang': 'Cộng đồng Front-end(HTML/CSS/JS) Việt Nam'
     },
 
 ];
@@ -60,3 +100,23 @@ var data = [
         'avatar': 'https://scontent.fdad3-1.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?_nc_cat=1&ccb=1-7&_nc_sid=2b6aad&_nc_ohc=-tIpWnFaNRwAX93Vkst&_nc_ht=scontent.fdad3-1.fna&oh=00_AfAZNfwMtzzcu6DFnes7xjGKHlaepDBjaSsMGGzNW3aj-Q&oe=6502AE78',
     },
 ];
+
+var seeMore = document.querySelector('.see-more');
+var shortCut = document.querySelector('#shortcut')
+
+var contentShortCut = "";
+
+
+$(shortcut_page).each(function (index, value) {
+    contentShortCut = contentShortCut + '<a href="#" class="text-decoration-none text-dark">';
+    contentShortCut = contentShortCut + ' <div class=" mt-2 mb-2 categorys">';
+    contentShortCut = contentShortCut + '<div class="d-flex align-items-center category rounded py-2" style="width: 100%;">';
+    contentShortCut = contentShortCut + '<img class="ms-2" src="'+value.hinh_anh+'" width="8%" alt="">';
+    contentShortCut = contentShortCut + '<span class="ms-3"><b>'+value.tieu_de_trang+'</b></span>';
+    contentShortCut = contentShortCut + '</div>';
+    contentShortCut = contentShortCut + '</div>';
+    contentShortCut = contentShortCut + '</a>';
+});
+
+$('#shortcut').html(contentShortCut);
+
